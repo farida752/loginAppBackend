adfa5f65978ade8f708a036b7a097193c9cbd56c
farida752 <fere.2000.24@gmail.com>
2021-11-05 15:41:00 +0200

    make it better

---
+++ app/Controllers/Http/SignController.js
@@ -0,0 +1,61 @@
+'use strict'
+
+
+const client = use('App/Models/Client')
+class SignController {
+    async addUser({request,response}){
+      if(request.body.email === null){
+        response.send({status : false, msg : "No email found!"});
+        return;
+      }
+      if(request.body.password === null && request.body.googleAuth === false){
+        response.send({status : false, msg : "No password found!"});
+        return;
+      }
+      const currentClientIds = await client.query().where('email', request.body.email).ids();
+        if(currentClientIds.length != 0) {// already exists // check if it comes from google{\
+            if(request.body.googleAuth == false){
+           response.send({status : false, msg : "emailExists!"});
+           }
+            else if(request.body.googleAuth == true){
+              response.send({status : true, msg : "success!"});
+            }
+            return;
+        }
+        const newClient = new client();
+        newClient .email =  request.body.email;
+        newClient.password = request.body.password;
+        await newClient.save();
+      response.send({status : true, msg : "success!"});
+    }
+
+    async signin({request,response}){
+      if(request.body.email === null){
+        response.send({status : false, msg : "No email found!"});
+        return;
+      }
+      if(request.body.password === null){
+        response.send({status : false, msg : "No password found!"});
+        return;
+      }
+        const currentClientIds = await client.query().where('email', request.body.email).ids();
+        if(currentClientIds.length != 0){
+        const currentClient = await client .find(currentClientIds);
+        if(currentClient.password === request.body.password){
+            response.send({status : true, msg : "success!"});
+            return;
+        }
+      }
+      
+       response.send({status:false,msg:"incorrect!",ids:currentClientIds,size:currentClientIds.length});
+    }
+   
+    async showUsers({reruest,response}){
+        const clients = await client.all();
+        response.send(clients);
+}
+async remove({request,response}){
+  const currentClientIds = await client.query().where('email', request.body.email).delete();
+}
+}
+module.exports = SignController
---
+++ start/routes.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.post('/signin','SignController.signin')
+Route.post('/signup','SignController.addUser');
+
+Route.get('/show','SignController.showUsers');
+
+Route.delete('/delete','SignController.remove');
